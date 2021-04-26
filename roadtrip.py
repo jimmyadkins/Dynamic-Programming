3cc03b0c6dd64602c8457a9865b7087d04cab537
jimmyadkins <56000625+jimmyadkins@users.noreply.github.com>
2021-04-26 09:13:37 -0500

    4/26 finished roadtrip

--- roadtrip.py
+++ roadtrip.py
@@ -3,33 +3,29 @@ def pen(source, destination):
 
 def roadtrip(arr):
     n = len(arr)
-
     cost = [0] * n
     last = [0] * n
-    for i in range(n-1, 0, -1):
-        cost[i] = pen(0, arr[i])
-        print(cost)
-        print(last)
-        for j in range(0, i-1):
-            penalty = pen(arr[j], arr[i])
+    for i in range(1, n):
+        cost[i] = pen(arr[last[i]], arr[i]) + cost[last[i]]
+        for j in range(0, i):
+            penalty = pen(arr[j], arr[i]) + cost[j]
             if penalty < cost[i]:
                 cost[i] = penalty
                 last[i] = j
-        i = last[i]
-    print(cost)
-    print(last)
 
-    # final_path = []
-    # final_path.append(n)
-    # final_path.append(last[n])
+    finalPath = [n-1]
+    while finalPath[-1]!= 0:
+        finalPath.append(last[finalPath[-1]])
+    finalPath.reverse()
+    return cost[-1], finalPath
 
-test_arr = [0, 175, 215, 280, 400, 450, 560, 640, 780, 820, 1000, 1060, 1140, 1190, 1260, 1350]
+# test_arr = [0, 175, 215, 280, 400, 450, 560, 640, 780, 820, 1000, 1060, 1140, 1190, 1260, 1350]
+test_arr = [0, 195, 212, 370, 450, 530, 670, 700, 800, 920, 970, 1085, 1130, 1220, 1350, 1440, 1600]
+
 result = roadtrip(test_arr)
 print()
-print()
-# print(result[0])
-# print(result[1])
-# print(result[2])
+print(result)
+
 
     # pen = [[(200 - arr[y] - arr[x]) * (200 - arr[y] - arr[x]) for x in range(n)] for y in range(n)]
     # print(pen)
